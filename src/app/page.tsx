0825adc5618713c061247108cf68f535ef6d7b5b
Muktadirul675 <muktadirul.05@gmail.com>
2025-01-01 01:02:19 +0000

    prisma debugged

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import { prisma } from "@/prisma"
 
 export default async function Home() {
-  const users = await prisma.user.findMany()
   return (
     <>
       Home
-      {JSON.stringify(users,null,2)}
     </>
   );
 }
